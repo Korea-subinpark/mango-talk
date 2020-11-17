daec84c3b044d87024779f60efee504ac2512ad2
Korea-subinpark <subinpark.re@gmail.com>
2020-11-17 20:29:14 +0900

    WebSocket 테스트 페이지 구현

--- mango-server/src/main/java/com/messenger/mango/common/config/WebSocketConfig.java
+++ mango-server/src/main/java/com/messenger/mango/common/config/WebSocketConfig.java
@@ -12,13 +12,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/mango");
-        config.setApplicationDestinationPrefixes("/mango");
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) { // endpoint를 등록하여 WS 사용이 불가능한 경우 대체 전송을 사용할 수 있도록 SockJS 풀백 옵션 활성화
-        registry.addEndpoint("/")
+        registry.addEndpoint("/stomp")
                 .setAllowedOrigins("*")
                 .withSockJS();
     }
--- mango-server/src/main/java/com/messenger/mango/web/ChatController.java
+++ mango-server/src/main/java/com/messenger/mango/web/ChatController.java
@@ -5,18 +5,23 @@ import com.messenger.mango.web.dto.ChatDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/mango/v1/chat")
 @RestController
 public class ChatController {
 
     private final ChatService chatService;
 
-    @MessageMapping("/v1/hello")
-    @SendTo("/mango/chat")
+    @MessageMapping("/echo")
+    @SendTo("/topic/echo")
+    public String echo(String message) {
+        System.out.println(message);
+        return message;
+    }
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/chat")
     public ChatDto.Response send(ChatDto.SaveRequest request) {
         Long chatRoomId = chatService.send(request);
 
