9808c2a2c395b07f6acab99d26674d4041c943ca
Korea-subinpark <subinpark.re@gmail.com>
2020-11-24 22:09:22 +0900

    채팅 테스트 구현

--- mango-server/src/main/java/com/messenger/mango/web/ChatController.java
+++ mango-server/src/main/java/com/messenger/mango/web/ChatController.java
@@ -4,7 +4,7 @@ import com.messenger.mango.service.chat.ChatService;
 import com.messenger.mango.web.dto.ChatDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -12,23 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
     private final ChatService chatService;
-
-    @MessageMapping("/echo")
-    @SendTo("/topic/echo")
-    public String echo(String message) {
-        System.out.println(message);
-        return message;
-    }
+    private final SimpMessagingTemplate template;
 
     @MessageMapping("/chat")
-    @SendTo("/topic/chat")
-    public ChatDto.Response send(ChatDto.SaveRequest request) {
-        Long chatRoomId = chatService.send(request);
+    public void send(ChatDto.SaveRequest request) {
+        chatService.send(request);
 
-        return ChatDto.Response.builder()
+        Long chatRoomId = request.getChatRoomId();
+        ChatDto.Response response = ChatDto.Response.builder()
                 .content(request.getContent())
                 .senderName(request.getSenderName())
                 .chatRoomId(chatRoomId)
                 .build();
+
+        template.convertAndSend("/topic/chat/" + chatRoomId, response);
     }
 }
