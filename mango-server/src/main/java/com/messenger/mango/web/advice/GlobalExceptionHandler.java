849748a6f865140da31c5952009298fda697ce66
Korea-subinpark <subinpark.re@gmail.com>
2021-02-05 14:48:24 +0900

    Post 관련 클래스 삭제

--- mango-server/src/main/java/com/messenger/mango/web/advice/GlobalExceptionHandler.java
+++ mango-server/src/main/java/com/messenger/mango/web/advice/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.messenger.mango.web.advice;
 
 import com.messenger.mango.common.ErrorCode;
 import com.messenger.mango.common.exception.NotFoundException;
-import com.messenger.mango.common.exception.PostNotFoundException;
 import com.messenger.mango.web.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +15,7 @@ import static org.springframework.http.ResponseEntity.status;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({PostNotFoundException.class, UsernameNotFoundException.class, NotFoundException.class})
+    @ExceptionHandler({UsernameNotFoundException.class, NotFoundException.class})
     public ResponseEntity<ErrorResponse> notFoundExceptionHandler(Exception ex) {
         return status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(ErrorCode.NOT_FOUND, ex.getMessage()));
